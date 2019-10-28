742733ddddc4f1eb1c250307c075d2cd3f878cf1
Christophe DAVID <christophe.david@onera.fr>
2019-10-28 15:01:01 +0100

    Initiated new geometry module

---
+++ src/fastoad_cs25/models/geometry/base.py
@@ -0,0 +1,28 @@
+"""
+Module for basic geometrical data
+"""
+
+
+#  This file is part of FAST-OAD_CS25
+#  Copyright (C) 2022 ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+
+class ComponentGeometry:
+    """A class for storing geometry data of an aircraft component"""
+
+    def __init__(self):
+        self.reference_length = None
+        """ Reference length of the element """
+
+        self.reference_area = None
+        """ Reference area of the element """
