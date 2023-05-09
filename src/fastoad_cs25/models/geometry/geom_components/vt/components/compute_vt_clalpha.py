1afab2ae537e85ed74f47ea72fd5b1ca91de8939
Christophe DAVID <christophe.david@onera.fr>
2023-05-09 06:23:32 +0200

    Fixed unit of CL_alpha

--- src/fastoad_cs25/models/geometry/geom_components/vt/components/compute_vt_clalpha.py
+++ src/fastoad_cs25/models/geometry/geom_components/vt/components/compute_vt_clalpha.py
@@ -2,7 +2,7 @@
     Estimation of vertical tail lift coefficient
 """
 #  This file is part of FAST-OAD_CS25
-#  Copyright (C) 2022 ONERA & ISAE-SUPAERO
+#  Copyright (C) 2023 ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -33,7 +33,7 @@ class ComputeVTClalpha(om.ExplicitComponent):
         self.add_input("data:geometry:vertical_tail:aspect_ratio", val=np.nan)
         self.add_input("data:geometry:vertical_tail:sweep_25", val=np.nan, units="deg")
 
-        self.add_output("data:aerodynamics:vertical_tail:cruise:CL_alpha")
+        self.add_output("data:aerodynamics:vertical_tail:cruise:CL_alpha", units="1/rad")
 
     def setup_partials(self):
         self.declare_partials("data:aerodynamics:vertical_tail:cruise:CL_alpha", "*", method="fd")
