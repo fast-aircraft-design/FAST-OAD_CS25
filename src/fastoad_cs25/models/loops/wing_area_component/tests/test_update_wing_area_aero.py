117560000680e39fc69d5c77e62def368aeff5c8
Christophe DAVID <christophe.david@onera.fr>
2024-03-08 11:10:56 +0100

    Added unit tests for wing area computation submodels.

---
+++ src/fastoad_cs25/models/loops/wing_area_component/tests/test_update_wing_area_aero.py
@@ -0,0 +1,52 @@
+"""
+test module for wing area computation
+"""
+#  This file is part of FAST-OAD_CS25
+#  Copyright (C) 2024 ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import openmdao.api as om
+from fastoad._utils.testing import run_system
+from numpy.testing import assert_allclose
+from openmdao.utils.assert_utils import assert_check_partials
+
+from ..update_wing_area_aero import UpdateWingAreaAero, WingAreaConstraintsAero
+
+
+def test_update_wing_area_aero():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:TLAR:approach_speed", val=132, units="kn")
+    ivc.add_output("data:weight:aircraft:MLW", val=66300, units="kg")
+    ivc.add_output("data:aerodynamics:aircraft:landing:CL_max", val=2.8)
+    ivc.add_output("data:geometry:wing:area", val=2.80)
+
+    problem = run_system(UpdateWingAreaAero(), ivc)
+    assert_allclose(problem["wing_area:aero"], 124.38, atol=1e-2)
+
+    data = problem.check_partials(out_stream=None)
+    assert_check_partials(data, atol=1, rtol=1e-4)
+
+
+def test_wing_area_constraints_aero():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:TLAR:approach_speed", val=132, units="kn")
+    ivc.add_output("data:weight:aircraft:MLW", val=66300, units="kg")
+    ivc.add_output("data:aerodynamics:aircraft:landing:CL_max", val=2.8)
+    ivc.add_output("data:geometry:wing:area", val=116.09, units="m**2")
+
+    problem = run_system(WingAreaConstraintsAero(), ivc)
+    assert_allclose(
+        problem["data:aerodynamics:aircraft:landing:additional_CL_capacity"], -0.2, atol=1e-2
+    )
+
+    data = problem.check_partials(out_stream=None)
+    assert_check_partials(data, atol=1, rtol=1e-4)
---
+++ src/fastoad_cs25/models/loops/wing_area_component/tests/test_update_wing_area_geom.py
@@ -0,0 +1,48 @@
+"""
+test module for wing area computation
+"""
+#  This file is part of FAST-OAD_CS25
+#  Copyright (C) 2024 ONERA & ISAE-SUPAERO
+#  FAST is free software: you can redistribute it and/or modify
+#  it under the terms of the GNU General Public License as published by
+#  the Free Software Foundation, either version 3 of the License, or
+#  (at your option) any later version.
+#  This program is distributed in the hope that it will be useful,
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  GNU General Public License for more details.
+#  You should have received a copy of the GNU General Public License
+#  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+import openmdao.api as om
+from fastoad._utils.testing import run_system
+from numpy.testing import assert_allclose
+from openmdao.utils.assert_utils import assert_check_partials
+
+from ..update_wing_area_geom import UpdateWingAreaGeom, WingAreaConstraintsGeom
+
+
+def test_update_wing_area_geom():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:geometry:wing:aspect_ratio", 9.48)
+    ivc.add_output("data:geometry:wing:root:thickness_ratio", 0.15)
+    ivc.add_output("data:geometry:wing:tip:thickness_ratio", 0.11)
+    ivc.add_output("data:weight:aircraft:sizing_block_fuel", val=20500, units="kg")
+
+    problem = run_system(UpdateWingAreaGeom(), ivc)
+    assert_allclose(problem["wing_area:geom"], 133.97, atol=1e-2)
+
+    data = problem.check_partials(out_stream=None)
+    assert_check_partials(data, atol=1, rtol=1e-4)
+
+
+def test_wing_area_constraints_geom():
+    ivc = om.IndepVarComp()
+    ivc.add_output("data:weight:aircraft:sizing_block_fuel", val=15000.0, units="kg")
+    ivc.add_output("data:weight:aircraft:MFW", val=12000.0, units="kg")
+
+    problem = run_system(WingAreaConstraintsGeom(), ivc)
+    assert_allclose(problem["data:weight:aircraft:additional_fuel_capacity"], -3000.0, atol=1e-2)
+
+    data = problem.check_partials(out_stream=None)
+    assert_check_partials(data, atol=1, rtol=1e-4)
