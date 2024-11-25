fbc6418fa8a42ec5a4398219ca8f2348a8c4fd19
Enrico Stragiotti <enrico.stragiotti@onera.fr>
2024-11-25 11:04:38 +0100

    WIP

--- src/fastoad_cs25/models/weight/mass_breakdown/update_mlw_and_mzfw.py
+++ src/fastoad_cs25/models/weight/mass_breakdown/update_mlw_and_mzfw.py
@@ -2,7 +2,7 @@
 Main component for mass breakdown
 """
 #  This file is part of FAST-OAD_CS25
-#  Copyright (C) 2022 ONERA & ISAE-SUPAERO
+#  Copyright (C) 2025 ONERA & ISAE-SUPAERO
 #  FAST is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
@@ -14,14 +14,18 @@ Main component for mass breakdown
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
+import fastoad.api as oad
 import numpy as np
 from openmdao.core.explicitcomponent import ExplicitComponent
 
+from .constants import SERVICE_MLW_MZFW
 
+
+@oad.RegisterSubmodel(SERVICE_MLW_MZFW, "fastoad.submodel.weight.mass.mlw_mzfw.legacy")
 class UpdateMLWandMZFW(ExplicitComponent):
     """
     Computes Maximum Landing Weight and Maximum Zero Fuel Weight from
-    Overall Empty Weight and Maximum Payload.
+    Overall Empty Weight and Maximum Payload, using an empirical law.
     """
 
     def setup(self):
@@ -43,6 +47,39 @@ class UpdateMLWandMZFW(ExplicitComponent):
 
         mzfw = owe + max_pl
         mlw = mlw_mzfw_ratio * mzfw
+
+        outputs["data:weight:aircraft:MZFW"] = mzfw
+        outputs["data:weight:aircraft:MLW"] = mlw
+
+
+@oad.RegisterSubmodel(SERVICE_MLW_MZFW, "fastoad.submodel.weight.mass.mlw_mzfw.analytical")
+class UpdateMLWandMZFWanalytical(ExplicitComponent):
+    """
+    Computes Maximum Landing Weight and Maximum Zero Fuel Weight from
+    max payload, reserve fuel weight and diversion fuel weight
+
+    """
+
+    def setup(self):
+        self.add_input("data:weight:aircraft:OWE", val=np.nan, units="kg")
+        self.add_input("data:weight:aircraft:max_payload", val=np.nan, units="kg")
+        self.add_input("data:mission:sizing:reserve:fuel", val=np.nan, units="kg")
+        self.add_input("data:mission:sizing:diversion:fuel", val=np.nan, units="kg")
+
+        self.add_output("data:weight:aircraft:MZFW", units="kg")
+        self.add_output("data:weight:aircraft:MLW", units="kg")
+
+    def setup_partials(self):
+        self.declare_partials("*", "*", method="fd")
+
+    def compute(self, inputs, outputs, discrete_inputs=None, discrete_outputs=None):
+        owe = inputs["data:weight:aircraft:OWE"][0]
+        max_pl = inputs["data:weight:aircraft:max_payload"][0]
+        fuel_reserve = inputs["data:mission:sizing:reserve:fuel"][0]
+        divert_fuel = inputs["data:mission:sizing:diversion:fuel"]
+
+        mzfw = owe + max_pl
+        mlw = mzfw + fuel_reserve + divert_fuel
 
         outputs["data:weight:aircraft:MZFW"] = mzfw
         outputs["data:weight:aircraft:MLW"] = mlw
